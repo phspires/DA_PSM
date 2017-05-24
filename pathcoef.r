c90fed690acf5ad912d08a3950681fd99c0cb380
Bruno Santos <Bruno Santos>
2017-05-24 21:00:20 +0100

    added path coefficient file and function

--- pathcoef.r
+++ pathcoef.r
@@ -1,26 +1,24 @@
-path.coef= function (LV,innermodel){
-  
+path.coef = function (LV, innermodel) {
   x = innermodel[1:nrow(innermodel), 2:ncol(innermodel)]
   Y.prec.index = c()
-  y=matrix(0,ncol(LV),ncol(LV))
+  y = matrix(0, ncol(LV), ncol(LV))
+  f = NULL
   
-  for ( i in (1:ncol(x))){
-  
-    for (j in (1:ncol(x))){
-     
-      if (x[j,i] ==1 ) {
-        print(paste("i: ",i,"j: ",j))
-        Y.prec.index=c(Y.prec.index,j)
+  for (i in (1:ncol(x))) {
+    for (j in (1:ncol(x))) {
+      if (x[j, i] == 1) {
+        Y.prec.index = c(Y.prec.index, j)
       }
-     
+      
     }
     
-    #print(Y.prec.index)
-    f=solve(t(LV[,Y.prec.index])%*%LV[,Y.prec.index])%*%t(LV[,Y.prec.index])%*%LV[,i]
+    if (!is.null(Y.prec.index)) {
+      f = solve(t(LV[, Y.prec.index]) %*% LV[, Y.prec.index]) %*% t(LV[, Y.prec.index]) %*%
+        LV[, i]
+    }
+    y[Y.prec.index, i] <- f
     Y.prec.index <- NULL
-     #print(f)
-   y[,i]=f
-  
+    
   }
- return(y)
+  return(y)
 }
