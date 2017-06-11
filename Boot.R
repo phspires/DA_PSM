617b1fe1f54021fc0cb1be9dff4427b33751827c
BrunoEduardoCSantos <brunopevide@gmail.com>
2017-06-11 20:04:38 +0100

    bootstrap t-statistic

--- Boot.R
+++ Boot.R
@@ -1,4 +1,5 @@
 source("code.R")
+source("w_matrix.R")
 bootstrap.matrix.sample<-function (data){
 
   n_rows = nrow(data)
@@ -7,23 +8,34 @@ bootstrap.matrix.sample<-function (data){
   return (ind)
 }
 
-bootstrap.matrix.statistic<-function (data,n_samples = 10,innerm,outerm){
+bootsrap.statistic<-function (data,n_samples = 10,innerm,outerm){
   
   sample_matrix <- matrix(NA,n_samples,nrow(innerm)+nrow(outerm))
-  
+  results <- list(mean = NULL, sd= NULL)
+  class(results) <-"bootstrap.matrix.statistic" 
   # Compute statistics and mean it to get statistic's value
   for (i in 1:n_samples) {
-    model<-advance.analytics.pls(data[bootstrap.matrix.sample(data),],innerm,outerm,tolerance = 0.00001)
-    val<-c(model$outer_weights[model$outer_weights!=0],model$path_coefficients[model$path_coefficients!=0])
+    model<-advance.analytics.pls(data[bootstrap.matrix.sample(data),],innerm,outerm,tolerance = 1e-7)
+    outer_loading<- create.w.matrix(outerm)*model$cross_loadings
+    val<-c(outer_loading[outer_loading!=0],model$path_coefficients[model$path_coefficients!=0])
     print(length(val))
-    #sample_matrix[i,]<- c(model$outer_weights[model$outer_weights!=0],model$path_coefficients[model$path_coefficients!=0])
+    sample_matrix[i,]<-val
   }
    
   mean<- colMeans(sample_matrix)
   #compute standard deviation
-  sd<- apply(sample_matrix, 1:ncol(sample_matrix), sd)
+  sd<- apply(sample_matrix, 2, sd)
+  results$mean<-mean
+  results$sd<-sd
   
   #compute tvalue
-  return (sd)
+  return (results)
   
+}
+#t statistitc
+bootstrap.tstat <- function(result,model,outerm,n_samples){
+  outer_loading<- create.w.matrix(outerm)*model$cross_loadings
+  val<-c(outer_loading[outer_loading!=0],model$path_coefficients[model$path_coefficients!=0])
+  t<- ((result$mean-val)*sqrt(n_samples))/(result$sd)
+  return(t)
 }
